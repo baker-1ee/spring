46ad7344a88a6fdba3b712917339b7168b511797
zeemoong <ljw722007@gmail.com>
2022-10-10 18:38:15 +0900

    md 제거

--- src/main/java/com/zeemoong/config/WebMvcConfig.java
+++ src/main/java/com/zeemoong/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    final LoggingInterceptor loggingInterceptor;
+    private final LoggingInterceptor loggingInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
